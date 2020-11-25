8bbce0ed4af65b00605d1405c71471e33f1783be
Kovacs Sandor <sada93@citromail.hu>
2020-11-25 08:02:38 +0100

    task2

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+$(document).ready(function(){
+    $("button").click(function(){
+      $("p").hide();
+    });
+  });
+
+
+
+
+/*
+----TASK 2----
+----TASK 1----
 $("#criminal").click(function(){
     $(this).hide();
-  });
+  });*/
